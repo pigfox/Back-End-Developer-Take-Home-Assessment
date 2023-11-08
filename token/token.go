fab6a249bc1b0e76c78debf1ee02bb56ec0c8d48
Peter Sjolin <pigfoxeria@gmail.com>
2023-11-08 15:18:10 -0800

    removed unused function

--- token/token.go
+++ token/token.go
@@ -3,7 +3,6 @@ package token
 import (
 	"bedtha/config"
 	"bedtha/structs"
-	"errors"
 	"fmt"
 	"github.com/dgrijalva/jwt-go"
 	"time"
@@ -21,21 +20,4 @@ func New(email string) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	tokenStr, err := token.SignedString([]byte(structs.JwtKey.Value))
 	return tokenStr, err
-}
-
-func IsValid(token string) (bool, error) {
-	claims := &structs.Claims{}
-	tkn, err := jwt.ParseWithClaims(token, claims, func(token *jwt.Token) (interface{}, error) {
-		return []byte(structs.JwtKey.Value), nil
-	})
-
-	if err != nil {
-		return false, err
-	}
-
-	if tkn == nil || !tkn.Valid {
-		return false, errors.New("invalid token")
-	}
-
-	return true, nil
 }
